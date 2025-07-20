8a148821b890be2bd37382bc2edd322008e6e1f6
MohamedArshath10 <arshath.m2003@gmail.com>
2025-07-20 19:51:52 +0530

    COmpleted Binary search

--- Leetcode/BinarySearch.py
+++ Leetcode/BinarySearch.py
@@ -1,10 +1,17 @@
-num = [-1, 0, 3, 5, 9 ,12]
-target = 10
-found = False
-for i in range(len(num)):
-    if num[i] == target:
-        print(i)
-        found = True
-        break
-if not found:
-    print(-1)
+def binary_search():
+    nums = [-1, 0, 3, 5, 9 ,12]
+    target = 10
+    start = 0
+    end = len(nums)-1
+    while start <= end:
+        mid = (start + end) // 2
+        num = nums[mid]
+        if num == target:
+            return mid
+        elif num < target:
+            start = mid + 1
+        elif num > target:
+            end = mid - 1
+    return -1
+
+print(binary_search())
