c9515ca8af294170d170c612d46224e034651fca
MohamedArshath10 <arshath.m2003@gmail.com>
2025-07-22 15:48:30 +0530

    Iris dataset  completed

--- AIML Intern/Machine_Learning/ir!s_datast.py
+++ AIML Intern/Machine_Learning/ir!s_datast.py
@@ -1,7 +1,16 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 df['target'] = iris.target
 print(df.head())
+
+# Splitting data
+print("Splitting data ---->")
+x = df.drop('target', axis=1)
+y = df['target']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+print("Train size: ", x_train.shape)
+print("Test size: ", x_test.shape)
