4fdeca488bd958401b6f74963e4e629d0cce4737
MohamedArshath10 <arshath.m2003@gmail.com>
2025-08-10 11:57:25 +0530

    WOrking on tuning

---
+++ Leetcode/Duplicate.py
@@ -0,0 +1,15 @@
+def duplicate(nums):
+    new_num = []
+    right = len(nums) - 1
+    while right >= 0:
+        if nums[right] not in new_num:
+            new_num.append(num[right])
+            nums.pop()
+            right -= 1
+        if nums[right] in new_num:
+            nums.pop()
+            right -= 1
+    new_num.reverse()
+    return new_num
+num = [1,1,2,3,3,4,5,5]
+print(duplicate(num))
